04d093b006331394bb9265dd7b3713b22fb64d0c
KittenCN <owdely@vip.163.com>
2023-03-20 08:42:09 +0800

    change bias in linear to true

--- common.py
+++ common.py
@@ -115,8 +115,8 @@ class LSTM(nn.Module):
     def __init__(self,dimension):
         super(LSTM,self).__init__()
         self.lstm=nn.LSTM(input_size=dimension,hidden_size=128,num_layers=3,batch_first=True)
-        self.linear1=nn.Linear(in_features=128,out_features=16,bias=False)
-        self.linear2=nn.Linear(16,OUTPU_DIMENSION,bias=False)
+        self.linear1=nn.Linear(in_features=128,out_features=16)
+        self.linear2=nn.Linear(16,OUTPU_DIMENSION)
         self.LeakyReLU=nn.LeakyReLU()
         self.ELU = nn.ELU()
     def forward(self,x):
