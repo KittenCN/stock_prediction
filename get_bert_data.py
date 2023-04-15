25fba603f482f755d7f3721bee3f6e8f571be366
KittenCN <owdely@vip.163.com>
2023-04-15 09:02:08 +0800

    add get bert method in bert dataset

---
+++ get_bert_data.py
@@ -0,0 +1,7 @@
+import datasets
+from init import *
+
+my_dataset_all = datasets.load_dataset(path='seamew/ChnSentiCorp', cache_dir=bert_data_path+'/data/')  # 获取整个数据集
+my_dataset_train = my_dataset_all['train']
+my_dataset_validation = my_dataset_all['validation']
+my_dataset_test = my_dataset_all['test']
