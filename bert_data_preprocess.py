e597a6a7b02fce5ddae2763962b1098cac0df125
KittenCN <owdely@vip.163.com>
2023-04-16 08:10:40 +0800

    support new type

--- bert_data_preprocess.py
+++ bert_data_preprocess.py
@@ -3,6 +3,7 @@ train_first_line = True
 x_list = []
 y_list = []
 
+## ----type I
 texts = pd.read_csv(open(bert_data_path+'/data'+'/Train_DataSet.csv',encoding='UTF-8'))
 labels = pd.read_csv(open(bert_data_path+'/data'+'/Train_DataSet_Label.csv',encoding='UTF-8'))
 
@@ -27,3 +28,14 @@ pbar.close()
 
 data = pd.DataFrame({'label':y_list,'text':x_list})
 data.to_csv(bert_data_path+'/data'+'/Train2.csv',index=False,sep=',',encoding='utf-8')
+
+##----type II
+negative = open(bert_data_path+'/data'+'/negative.txt',encoding='UTF-8').readlines()
+positive = open(bert_data_path+'/data'+'/positive.txt',encoding='UTF-8').readlines()
+negative = map(lambda x: x.strip(), negative)
+positive = map(lambda x: x.strip(), positive)
+df_neg = pd.DataFrame({'label':0,'text':negative})
+df_pos = pd.DataFrame({'label':1,'text':positive})
+df = pd.concat([df_pos,df_neg],axis=0)
+df.to_csv(bert_data_path+'/data'+'/Train3.csv',index=False,sep=',',encoding='utf-8')
+
