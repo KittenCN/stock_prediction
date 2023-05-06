09ce4cb67a939f9445469f92f3f1f555f55225c4
KittenCN <owdely@vip.163.com>
2023-05-06 14:52:33 +0800

    fix bug for the shape of tgt

--- common.py
+++ common.py
@@ -571,14 +571,14 @@ class TransformerModel(nn.Module):
         tgt_positions = torch.arange(tgt_seq_length, device=tgt.device).unsqueeze(1).expand(tgt_seq_length, src_batch_size)
         tgt = tgt_embedding + self.positional_encoding[tgt_positions]
 
-        output = self.transformer_decoder(tgt.transpose(0, 1), memory)
+        output = self.transformer_decoder(tgt, memory)
 
         if predict_days == 0:
-            output = output.permute(1, 0, 2)  # (batch_size, seq_len, d_model) -> (seq_len, batch_size, d_model)
-            pooled_output = self.pooling(1)(output.permute(0, 2, 1))
+            # output = output.permute(1, 0, 2)  # (batch_size, seq_len, d_model) -> (seq_len, batch_size, d_model)
+            pooled_output = self.pooling(output.permute(1,2,0)) # (seq_len, batch_size, d_model) -> (batch_size, d_model, seq_len) -> (batch_size, d_model, seq_len)
             output = self.fc(pooled_output.squeeze(2))
         else:
-            output = output.permute(0, 2, 1)  # (batch_size, seq_len, d_model) -> (batch_size, d_model, seq_len)
+            output = output.permute(1, 2, 0)  # (seq_len, batch_size, d_model) -> (batch_size, d_model, seq_len)
             pooled_output = self.pooling(predict_days)(output)
             output = self.fc(pooled_output.permute(0,2,1))
 
